4b501e8b67d4dd5259bf79df85d275e6b6128639
José Medeiro <jmedeiro02@gmail.com>
2021-05-07 22:10:19 +0100

    Text Update

--- t3/mat/FullStabilizer.m
+++ t3/mat/FullStabilizer.m
@@ -10,7 +10,7 @@ m         = 0;
 
 Resultado = 1:2;
 t     = linspace(0,200e-3,RESOLUCAO);
-vin_t = Vin*abs(real(e.^(j*50*2*pi*t)));
+vin_t = Vin*abs(real(e.^(j*(50*2*pi*t + pi/2))));
 for c=1:RESOLUCAO
   
   if (vin_t(c) < 0.75*2)
@@ -24,17 +24,34 @@ endfor
 
 %%  Vamos chamar o envelope
 
-ve_t    = Envelope(t, vin_t, R1, C1);
+ve_t_f    = Envelope(t, vin_t, R1, C1);
 
 %%  Vamos chamar o retificador
 
-vout_t  = Rectifier(t, ve_t, R2, n, m);
+vout_t_f  = Rectifier(t, ve_t_f, R2, n, m);
 
+%%  Retirar o intervalo de tempo que queremos
+c = 1;
+while t(c) < 10e-3
+  c = c +1;
+endwhile
+t_hold = c;
+t_g    = linspace(t(t_hold),200e-3,RESOLUCAO-t_hold+1);
+
+ve_t    = t_g;
+vout_t  = t_g;
+for c=t_hold:RESOLUCAO
+  
+  ve_t(c-t_hold+1)    = ve_t_f(c);
+  vout_t(c-t_hold+1)  = vout_t_f(c);
+  
+endfor
+  
 %Gráficos
 hf_PASSO4 = figure();
 hold on
-plot(t*1e3,ve_t)
-plot(t*1e3,vout_t)
+plot(t_g*1e3,ve_t)
+plot(t_g*1e3,vout_t)
 xlabel ("t, ms");
 ylabel ("Voltage, V");
 legend("v_{envelope}", "v_{out}");
@@ -43,7 +60,7 @@ hold off;
 
 hf_PASSO41 = figure();
 hold on
-plot(t*1e3,ve_t)
+plot(t_g*1e3,ve_t)
 xlabel ("t, ms");
 ylabel ("Voltage, V");
 legend("v_{envelope}");
@@ -52,7 +69,7 @@ hold off;
 
 hf_PASSO42 = figure();
 hold on
-plot(t*1e3,vout_t)
+plot(t_g*1e3,vout_t)
 xlabel ("t, ms");
 ylabel ("Voltage, V");
 legend("v_{out}");
@@ -61,7 +78,7 @@ hold off;
 
 hf_PASSO5 = figure();
 hold on
-plot(t*1e3,vout_t - 12)
+plot(t_g*1e3,vout_t - 12)
 xlabel ("t, ms");
 ylabel ("Voltage, V");
 legend("v_{out} - 12V");
